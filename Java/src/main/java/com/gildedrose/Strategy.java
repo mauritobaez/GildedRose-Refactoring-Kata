fced8a3ae6da589886876851dc4c6a1991982fec
MatyManzur <mmanzur@itba.edu.ar>
2025-03-26 19:53:19 +0100

    level 3: aged & backstage pass

--- Java/src/main/java/com/gildedrose/Strategy.java
+++
@@ -1,9 +0,0 @@
-package com.gildedrose;
-
-import com.gildedrose.items.Item;
-
-public interface Strategy {
-
-    void execute(Item item);
-
-}
---
+++ Java/src/main/java/com/gildedrose/items/BackstagePassesItem.java
@@ -0,0 +1,20 @@
+package com.gildedrose.items;
+
+public class BackstagePassesItem extends Item{
+    public BackstagePassesItem(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    @Override
+    public void UpdateQuality() {
+        if (this.sellIn <= 0) {
+            this.quality = 0;
+        } else if (this.sellIn <= 5) {
+            this.quality = Math.min(50, this.quality+3);
+        } else if (this.sellIn <= 10) {
+            this.quality = Math.min(50, this.quality+2);
+        } else {
+            this.quality = Math.min(50, this.quality+1);
+        }
+    }
+}
