6b5b4bb169b1ca168a3f5b05b81c6260b3d043d6
mauritobaez <mauroleanbaez@gmail.com>
2025-03-26 20:35:13 +0100

    BackstagePassesFactory

--- Java/src/main/java/com/gildedrose/factories/BackstagePassesFactory.java
+++ Java/src/main/java/com/gildedrose/factories/BackstagePassesFactory.java
@@ -1,26 +1,36 @@
 package com.gildedrose.factories;
 
+import com.gildedrose.items.BackstagePassesGalaItem;
 import com.gildedrose.items.BackstagePassesItem;
 import com.gildedrose.items.Item;
 
 public class BackstagePassesFactory extends Factory{
 
-    private String concertName;
+    private Concert concertType;
 
-    public BackstagePassesFactory(String concertName) {
-        this.concertName = concertName;
+    public BackstagePassesFactory(Concert concertType) {
+        this.concertType = concertType;
     }
 
     public BackstagePassesFactory() {
-        this.concertName = "TAFKAL80ETC";
+        this.concertType = Concert.TAFKAL80ETC;
     }
 
-    public void setConcertName(String concertName) {
-        this.concertName = concertName;
+    public void setConcertType(Concert concertType) {
+        this.concertType = concertType;
     }
-    
+
     @Override
     public Item createItem(int sellIn, int quality) {
-        return new BackstagePassesItem("Backstage passes to a"  + this.concertName + "concert", sellIn, quality);
+        switch (this.concertType) {
+            case GALA:
+                return new BackstagePassesGalaItem("Backstage passes to a GALA concert", sellIn, quality);
+            case TAFKAL80ETC:
+                return new BackstagePassesItem("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
+            default:
+                break;
+        }
+        throw new IllegalArgumentException("Backstage Passes Type not found :(");
+
     }
 }
